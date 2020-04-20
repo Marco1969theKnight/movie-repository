bb42714198ec4c97db49bb5fca3166fdb453d225
Marco1969theKnight <ma.diazvaldes@ugto.mx>
2020-04-20 09:50:03 -0500

    Primera prueba de insersion en la tabla generos

--- test.py
+++ test.py
@@ -1,5 +1,12 @@
 from Model.model import Model
 
 m = Model()
-print('Success')
+
+m.create_genero('Accion', 'Super Heroes')
+m.create_genero('Aventura', 'Exploracion')
+m.create_genero('Experimental', 'Indie')
+
+data = m.read_all_generos()
+print(data)
+
 m.close_db()
